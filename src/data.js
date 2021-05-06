9cca4f45598f69245b2108e5ed26a5a7ef40179d
navaneeth reddy <navaneethreddyboda@gmail.com>
2021-05-06 21:03:13 +0530

    carousels-in-react-part-3

---
+++ src/data.js
@@ -0,0 +1,8 @@
+import image1 from "./assets/carouselImage1.jpg";
+import image2 from "./assets/carouselImage2.jpg";
+import image3 from "./assets/carouselImage3.jpg";
+import image4 from "./assets/carouselImage4.jpg";
+import image5 from "./assets/carouselImage5.jpg";
+import image6 from "./assets/carouselImage6.jpg";
+
+export const data = [image1, image2, image3, image4, image5, image6];
