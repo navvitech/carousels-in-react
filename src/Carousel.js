b72df96c4c1a621c4ae5c276dc1bc8b41aefe8a8
navaneeth reddy <navaneethreddyboda@gmail.com>
2021-05-06 21:01:43 +0530

    carousels-in-react-part-3

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,37 +1,63 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import image1 from "./assets/carouselImage1.jpg";
-import image2 from "./assets/carouselImage2.jpg";
-import image3 from "./assets/carouselImage3.jpg";
-import image4 from "./assets/carouselImage4.jpg";
-import image5 from "./assets/carouselImage5.jpg";
-import image6 from "./assets/carouselImage6.jpg";
 import "./carousel.css";
+import { data } from "./data";
+import { ArrowBackIos, ArrowForwardIos } from "@material-ui/icons";
+
+const PreviousBtn = (props) => {
+  console.log(props);
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowBackIos style={{ color: "blue", fontSize: "30px" }} />
+    </div>
+  );
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+  return (
+    <div className={className} onClick={onClick}>
+      <ArrowForwardIos style={{ color: "blue", fontSize: "30px" }} />
+    </div>
+  );
+};
 
 const Carousel = () => {
   return (
     <div style={{ margin: "30px" }}>
       <h1>Basic carousel</h1>
-      <Slider>
-        <div>
-          <img src={image1} alt="" style={{ width: "100%", height: "50vh" }} />
-        </div>
-        <div>
-          <img src={image2} alt="" style={{ width: "100%", height: "50vh" }} />
-        </div>
-        <div>
-          <img src={image3} alt="" style={{ width: "100%", height: "50vh" }} />
-        </div>
-        <div>
-          <img src={image4} alt="" style={{ width: "100%", height: "50vh" }} />
-        </div>
-        <div>
-          <img src={image5} alt="" style={{ width: "100%", height: "50vh" }} />
-        </div>
-        <div>
-          <img src={image6} alt="" />
-        </div>
+      <Slider
+        autoplay
+        autoplaySpeed={2000}
+        // dots
+        initialSlide={2}
+        infinite
+        prevArrow={<PreviousBtn />}
+        nextArrow={<NextBtn />}
+        customPaging={(i) => {
+          return (
+            <div>
+              <img
+                src={data[i]}
+                alt=""
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  objectFit: "cover",
+                  borderRadius: "10px",
+                }}
+              />
+            </div>
+          );
+        }}
+        dotsClass="slick-dots custom-indicator"
+      >
+        {data.map((item) => (
+          <div>
+            <img src={item} alt="" style={{ width: "100%", height: "40vh" }} />
+          </div>
+        ))}
       </Slider>
     </div>
   );
